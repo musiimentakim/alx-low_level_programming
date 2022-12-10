2cd208a22f589cb44841081a5cfc31e41006118c
MUSIIMENTA <musiimentajoachim10@gmail.com>
2022-12-10 13:38:20 +0000

    print alphabet ,leave out e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entrypoint
+ * Description: 'print all except q and e'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letters;
+
+	for (letters = 'a'; letters < 'z'; letters++)
+	{
+		if (letters == 'q' || letters == 'e')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(letters);
+		}
+	}
+	putchar(*"\n");
+	return (0);
+}
