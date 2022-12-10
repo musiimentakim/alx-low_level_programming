6ead0eee25bfe8bb96e4f9aea43bdad6d5dcb778
MUSIIMENTA <musiimentajoachim10@gmail.com>
2022-12-10 11:52:36 +0000

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Entrypoint
+ *Description: 'listing alphabet using putchar'
+ *Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char letters[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i <= 26; i++)
+	{
+		putchar(letters[i]);
+	}
+	return (0);
+}
