aa4ca51cbb4202d8b683447c15ef93221beeb125
MUSIIMENTA <musiimentajoachim10@gmail.com>
2022-12-10 09:12:09 +0000

    Positive anything is better than negative nothing

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/*
+ *main - Entrypoint
+ *Description: 'integr status checker'
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n)
+	}
+	if else(n < 0)
+	{
+		printf("%d is negative\n", n)
+	}
+	else
+	{
+		printf("%d is zero\n", n)
+	}
+	return (0)
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+# 0x01.C-if ,else ,while
+This directory is made of c scripts for conditional execution
