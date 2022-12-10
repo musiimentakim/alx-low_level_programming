670826ee8a0cd659594d9bb4ca700ab730841e00
MUSIIMENTA <musiimentajoachim10@gmail.com>
2022-12-10 14:33:23 +0000

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entrypoint
+ * Description: 'print out integers from 0 to 9 using putchar'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number;
+
+	number = 0;
+	for (; number < 10; number++)
+	{
+		putchar(number);
+	}
+	putchar(*"\n");
+	return (0);
+}
