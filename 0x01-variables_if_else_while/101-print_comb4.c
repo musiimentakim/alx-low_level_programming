591dccc975e8736b3267c63d022053760ff685c5
MUSIIMENTA <musiimentajoachim10@gmail.com>
2022-12-12 08:44:05 +0000

    three digit combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entrypoint
+ * Description: 'Three combination digits'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num1, num2, num3;
+
+	for (num1 = 0; num1 < 8; num1++)
+	{
+		for (num2 = num1 + 1; num2 < 9; num2++)
+		{
+			for (num3 = num2 + 1; num3 < 10; num3++)
+			{
+				putchar(num1 + '0');
+				putchar(num2 + '0');
+				putchar(num3 + '0');
+				if (num1 == 7 && num2 == 8 && num3 ==)
+				{
+					continue;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
