c7982319f2d2fdfa52d3b731e3b673560de33b33
MUSIIMENTA <musiimentajoachim10@gmail.com>
2022-12-12 07:24:50 +0000

    combination of single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entrypoint
+ * Description: 'possible combination of single digit numbers'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers < 10; numbers++)
+	{
+		putchar(numbers + '0');
+		putchar(*",");
+		putchar(*" ");
+	}
+	putchar(*"\n");
+	return (0);
+}
