e33a26d51d753f79d39883e5a6a417b0a275a664
MUSIIMENTA <musiimentajoachim10@gmail.com>
2022-12-14 10:43:57 +0000

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Function for alphabet print out
+ * Description: 'print out ten lines of alphabet'
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	char b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
