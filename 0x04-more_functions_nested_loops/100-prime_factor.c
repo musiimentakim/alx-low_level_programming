c334908c7df2e134dbc2028763a087597d0c7487
MUSIIMENTA <musiimentajoachim10@gmail.com>
2022-12-16 21:16:52 +0000

    distinguish prime from composite numbers

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entrypoint
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	long a, b;
+
+	a = 612852475143;
+
+	for (b = 2; a > b; b++)
+	{
+		while (a % b == 0)
+		{
+			a = a / b;
+		}
+	}
+	printf("%lu", b);
+	putchar('\n');
+	return (0);
+}
