46fa3588a36f4ae3a6816833c10b51b73cb8cd42
musiimentakim <musiimentajoachim10@gmail.com>
2023-01-14 10:14:46 +0000

    Silence is argument carried out by other means mandatory

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Function
+ * @argc: parameter count
+ * @argv: parameter array
+ * Return: Always 0 for success and any positive integer otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
