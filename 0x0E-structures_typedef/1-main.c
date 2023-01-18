864bfe6d495beb2217864d19c18663c481713f7e
musiimentakim <musiimentajoachim10@gmail.com>
2023-01-18 02:55:56 +0000

    A dog is the only thing on earth that loves you more than you love yourself

---
+++ 0x0E-structures_typedef/1-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+	struct dog my_dog;
+	void init_dog(struct dog *d, char *name, float age, char *owner);
+
+	init_dog(&my_dog, "Poppy", 3.5, "Bob");
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+	return (0);
+}
