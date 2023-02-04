175cd549014dedc051a1cfaed664da75499cf2c8
musiimentakim <musiimentajoachim10@gmail.com>
2023-02-04 15:49:33 +0000

    correction

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,14 +5,14 @@
 /**
  * malloc_checked - function that allocates memory
  * @b: size of memory to allocate
- * Return: on success pointer to memory otherwise exit with status 98 
+ * Return: on success pointer to memory otherwise exit with status 98
  */
 
 void *malloc_checked(unsigned int b)
 {
 	void *pointer;
 
-	pointer = malloc(sizeof(b));
+	pointer = malloc(b);
 	if (pointer == NULL)
 	{
 		free(pointer);
